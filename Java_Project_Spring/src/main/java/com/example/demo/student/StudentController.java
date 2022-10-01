816daecbba77f379bd08399eebabffed604dead7
Tomas Cibulskas <65713715+Tomrasci@users.noreply.github.com>
2022-10-01 23:16:55 +0300

    added service layer

--- Java_Project_Spring/src/main/java/com/example/demo/student/StudentController.java
+++ Java_Project_Spring/src/main/java/com/example/demo/student/StudentController.java
@@ -4,21 +4,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
+
+    private  final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping
-    public List<Student> getStudents() {
-        return List.of(new Student(
-                1L,
-                "Mariam", "mariam.jamal@gmail.com",
-                18,
-                LocalDate.of(2000, Month.FEBRUARY, 5)
-        ));
+   public List<Student> getStudents() {
+        return studentService.getStudents();
     }
 
 }
---
+++ Java_Project_Spring/src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,16 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+public class StudentService {
+    public List<Student> getStudents() {
+        return List.of(new Student(
+                1L,
+                "Mariam", "mariam.jamal@gmail.com",
+                18,
+                LocalDate.of(2000, Month.FEBRUARY, 5)
+        ));
+    }
+}
